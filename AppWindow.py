896a95412d80410a4c47559f5d239a9289394c1c
TheNightJourney <anohinoyoji@gmail.com>
2022-11-24 18:51:15 +0700

    Initial Commit - Step 2 not working.

--- AppWindow.py
+++ AppWindow.py
@@ -1,17 +1,18 @@
 from register_or_login import *
-from Bank import Bank
+from Customer_Page import *
 
 
+# Entry text.
 entry_function()
+
+# To register or log in (the register loops to log in once finished).
 user = regis_or_log()
+print("\n")
 
-
-
-
-
-
-
-
-
-
+if user == "1":
+    print("Admin panel under construction.")
+elif user == "0":
+    customer_menu_panel()
+else:
+    print("Unknown user detected.", type(user), user)
 
---
+++ Customer_Page.py
@@ -0,0 +1,6 @@
+def customer_menu_panel():
+    print("=== What would you like to do? ===")
+    print("=== [0] Check Balance ===\n"
+          "=== [1] Make a Payment ===\n"
+          "=== [2] Make a Withdrawal ===\n"
+          "=== [3] Make a Deposit ===")
